37454743f899fd003d251954bc6b4799c40f6aff
Yevgeniy Pivnyak <dneprojek@gmail.com>
2020-04-16 20:38:37 +0300

    first task is done!

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-//var firstStep = prompt("Please enter Names, then / and pattern");
-
-var firstStep = "Roma,Anna,Sveta,Valera,/<p><b>$<b><p>";
-
-
+var firstStep = prompt("Please enter Names, then / and pattern. Without using space!");
 
 var mainArray = firstStep.split(',');
 
 var names = mainArray.slice(0, mainArray.length-1);;
-console.log(names);
 
-var myPattern1 = mainArray.slice(mainArray.length-1);
-
-
+function getPattern(mA){
+var myPattern1 = mA.slice(mainArray.length-1);
 var myPattern2 = myPattern1[0];
 var myPattern3 = myPattern2.split('')
 var rez = "";
@@ -21,11 +15,13 @@ for(var i of myPattern3){
     }
     rez += i;
 }
-
 var myPattern4 = rez.split('$')
+return myPattern4;
+}
+var Pattern = getPattern(mainArray);
 
-var startTag = myPattern4[0];
-var lastTag = myPattern4[1];
+var startTag = Pattern[0];
+var lastTag = Pattern[1];
 
 function creatorHTML (data,st,lt){
     var stringHTML = "";
@@ -36,6 +32,7 @@ function creatorHTML (data,st,lt){
 }
 
 var html = creatorHTML(names,startTag,lastTag);
-
 var elem = document.getElementById('names-container');
+
 elem.innerHTML = html;
+console.log(html);
