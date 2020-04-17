64f507c3001962d67d81f4d6f0a6b0cebd7e5f6c
Yevgeniy Pivnyak <dneprojek@gmail.com>
2020-04-17 23:15:31 +0300

    task-1 refactored

--- main.js
+++ main.js
@@ -2,23 +2,35 @@ var firstStep = prompt("Please enter Names, then / and pattern. Without using sp
 
 var mainArray = firstStep.split(',');
 
-var names = mainArray.slice(0, mainArray.length-1);;
+var names = mainArray.slice(0,mainArray.length-1);
 
-function getPattern(mA){
-var myPattern1 = mA.slice(mainArray.length-1);
-var myPattern2 = myPattern1[0];
-var myPattern3 = myPattern2.split('')
-var rez = "";
-for(var i of myPattern3){
-    if(i==="/"){
-    continue
-    }
-    rez += i;
+var Pattern1 = mainArray.slice(mainArray.length-1);
+var Pattern2 = Pattern1[0];
+var Pattern3 = Pattern2.split('/');
+
+names.push(Pattern3[0]);
+
+var Pattern4 = "";
+for (var i in Pattern2){
+   
+    if(Pattern2[i] != "$" && 
+    Pattern2[i] != "<" &&
+    Pattern2[i] != ">" &&
+    Pattern2[i] != "p" &&
+    Pattern2[i] != "b" &&
+    Pattern2[i] != "/" 
+    ) continue;
+    Pattern4 += Pattern2[i];
 }
-var myPattern4 = rez.split('$')
-return myPattern4;
+
+var pattern = Pattern4.slice(1);
+
+function getPattern(pa){
+var myPattern5 = pa.split('$')
+return myPattern5;
 }
-var Pattern = getPattern(mainArray);
+
+var Pattern = getPattern(pattern);
 
 var startTag = Pattern[0];
 var lastTag = Pattern[1];
@@ -35,4 +47,3 @@ var html = creatorHTML(names,startTag,lastTag);
 var elem = document.getElementById('names-container');
 
 elem.innerHTML = html;
-console.log(html);
